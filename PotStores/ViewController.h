ab72711068c996b26156b0bba6c53ea69a8a2982
Benson Huynh <quoc@shaw.ca>
2015-11-30 19:52:13 -0800

    first save

---
+++ PotStores/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PotStores
+//
+//  Created by Benson Huynh on 2015-11-30.
+//  Copyright © 2015 Benson Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PotStores/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  PotStores
+//
+//  Created by Benson Huynh on 2015-11-30.
+//  Copyright © 2015 Benson Huynh. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Parse/Parse.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+    testObject[@"foo"] = @"bar";
+    [testObject saveInBackground];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
